aa7737bec5ba1953b65c11d6cec00633614c5547
Jonah Foong <jonahfoong@gmail.com>
2024-08-29 19:40:55 +0200

    resolve params conflict

--- chicago_crime/params.py
+++ chicago_crime/params.py
@@ -1,5 +1,4 @@
 # add params here
-<<<<<<< HEAD
 import os
 
 GCP_PROJECT = os.environ.get("GCP_PROJECT")
@@ -7,8 +6,7 @@ BQ_DATASET = os.environ.get("BQ_DATASET")
 BQ_REGION = os.environ.get("BQ_REGION")
 GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 QUERY_NROWS = os.environ.get("QUERY_NROWS")
-=======
-import os 
+import os
 
 # Read the Databricks API token from the file
 with open('secrets/databricks_api.txt', 'r') as file:
@@ -17,4 +15,3 @@ with open('secrets/databricks_api.txt', 'r') as file:
 # Set the environment variables for Databricks
 os.environ['DATABRICKS_HOST'] = 'https://4095687731202574.4.gcp.databricks.com'
 os.environ['DATABRICKS_TOKEN'] = databricks_token
->>>>>>> origin/main
