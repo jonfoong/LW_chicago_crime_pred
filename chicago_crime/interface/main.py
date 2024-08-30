b4eff84cbf8434e824d3721948a57067737f713f
jonfoong <jonahfoong@gmail.com>
2024-08-30 16:16:01 +0200

    add training functionality

--- chicago_crime/interface/main.py
+++ chicago_crime/interface/main.py
@@ -1,36 +1,78 @@
 from chicago_crime.params import *
-from chicago_crime.ml_logic.extract import load_raw_data
+from chicago_crime.ml_logic.extract import load_raw_data, load_postproc_data
 from chicago_crime.ml_logic.transform import add_missing_communities, clean_data_frame
 from chicago_crime.ml_logic.load import upload_dt_to_bigquery
-from google.oauth2 import service_account
+from chicago_crime.ml_logic.model import initialize_model, train_model, compile_model
+from chicago_crime.ml_logic.registry import save_model
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
 
-# Load Credentials
-credentials = service_account.Credentials.from_service_account_file(GOOGLE_APPLICATION_CREDENTIALS)
 
 def preprocess_data() -> None:
-
-    # Add missing communities to the data
-    add_missing_communities(project_id = GCP_PROJECT, credentials = credentials )
+    
+    # Add missing communities to the data and upload to gbq
+    add_missing_communities()
 
     # Load Raw Data from Google Big Query. It is grouped by Days
-    df = load_raw_data(
-        credentials = credentials
-    )
-
-    print(df.info())
-    print(df.head(10))
-
+    df = load_raw_data()
     postproc_df = clean_data_frame(df)
     upload_dt_to_bigquery(postproc_df)
 
     print("✅ preprocess_data() done !!!\n")
 
 
-# TODO: train on preprocessed data
-
 def train():
-    pass
+
+    # get data from gcp
+
+    df = load_postproc_data()
+
+    # split train val
+
+    # get a smaller dataset first, expand later
+
+    df_small = df[pd.to_datetime(df.Date_day).dt.year==2024].query("community_area=='25'")
+
+    # specify sequence length and split data into train and val
+    sequence_length = 7
+    crime_count_list = list(df_small["crime_count"])
+    X, y = [], []
+    for i in range(sequence_length, len(crime_count_list)):
+        X.append(crime_count_list[i-sequence_length:i])
+        y.append(crime_count_list[i])
+
+    X = np.array(X)
+    y = np.array(y)
+
+    X_train, X_val, y_train, y_val = train_test_split(X, y,
+                                                      test_size=0.25, shuffle=False)    
+    # rescale data
+
+    scaler=MinMaxScaler()
+    scaler.fit(X_train)
+
+    X_train_scaled=scaler.transform(X_train)
+    X_val_scaled=scaler.transform(X_val)
+
+    # initialize model
+
+    model = initialize_model(sequence_length)   
+    model = compile_model(model)
+
+    # fit and train model
+
+    history, model = train_model(model, X_train_scaled, y_train, X_val_scaled, 
+                y_val, epochs = 10, batch_size = 16)    
+
+    mae = np.min(history.history['val_loss'])
+
+    # save model to mlflow
+
+    save_model(model, mae)
+    print("Model trained and saved to mlflow")
 
 # TODO: predict on new data
 
--- chicago_crime/params.py
+++ chicago_crime/params.py
@@ -1,17 +1,27 @@
+# load dotenv
+from dotenv import load_dotenv
+load_dotenv()
+
 # add params here
 import os
+
+# GCP
 
 GCP_PROJECT = os.environ.get("GCP_PROJECT")
 BQ_DATASET = os.environ.get("BQ_DATASET")
 BQ_REGION = os.environ.get("BQ_REGION")
-GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 QUERY_NROWS = os.environ.get("QUERY_NROWS")
-import os
+
+# databricks
+DATABRICKS_TOKEN = os.environ.get("DATABRICKS_TOKEN")
+DATABRICKS_EXP_ID = os.environ.get("DATABRICKS_EXP_ID")
+DATABRICKS_EXP_URI = os.environ.get("DATABRICKS_EXP_URI")
+DATABRICKS_EXP_PATH = os.environ.get("DATABRICKS_EXP_PATH")
 
 # Read the Databricks API token from the file
 with open('secrets/databricks_api.txt', 'r') as file:
     databricks_token = file.read().strip()
 
 # Set the environment variables for Databricks
-os.environ['DATABRICKS_HOST'] = 'https://4095687731202574.4.gcp.databricks.com'
 os.environ['DATABRICKS_TOKEN'] = databricks_token
+
